ac3f2633d960265c073a2d9580cca2ddf19d1392
Grudge69 <shubham.shubham.rawat090@gmail.com>
2023-08-20 21:52:09 +0530

    Account page + logout finished

---
+++ client/src/pages/AccountPage.jsx
@@ -0,0 +1,69 @@
+import { useContext, useState } from "react";
+import { UserContext } from "../UserContext";
+import { Link, Navigate, useParams } from "react-router-dom";
+import axios from "axios";
+
+export default function AccountPage() {
+  const [redirect, setRedirect] = useState(null);
+  const { user, ready, setUser } = useContext(UserContext);
+
+  let { subpage } = useParams();
+  if (subpage === undefined) {
+    subpage = "profile";
+  }
+
+  async function logout() {
+    await axios.post("/logout");
+    setRedirect("/");
+    setUser(null);
+  }
+
+  // If the user is not fetched yet, show LOADING
+  if (!ready) {
+    return "Loading...";
+  }
+
+  // No redirect to other page(like on logout we should redirect to '/' instead of '/login') && If the user is not present, redirect to LOGIN PAGE
+  if (!redirect && ready && !user) {
+    return <Navigate to={"/login"} />;
+  }
+
+  function linkClasses(type = null) {
+    let classes = "py-2 px-6";
+
+    if (type === subpage) {
+      classes += " bg-primary text-white rounded-full";
+    }
+
+    return classes;
+  }
+
+  if (redirect) {
+    return <Navigate to={redirect} />;
+  }
+
+  return (
+    <div>
+      <nav className="w-full flex justify-center mt-8 gap-2 mb-8">
+        <Link className={linkClasses("profile")} to={"/account"}>
+          My profile
+        </Link>
+        <Link className={linkClasses("bookings")} to={"/account/bookings"}>
+          My bookings
+        </Link>
+        <Link className={linkClasses("places")} to={"/account/places"}>
+          My accommodations
+        </Link>
+      </nav>
+
+      {subpage === "profile" && (
+        <div className="text-center max-w-lg mx-auto">
+          Logged in as {user.name} ({user.email}) <br />
+          <button onClick={logout} className="primary max-w-sm mt-2">
+            Logout
+          </button>
+        </div>
+      )}
+    </div>
+  );
+}
